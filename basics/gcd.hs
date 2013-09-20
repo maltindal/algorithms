7994aab41a22cc31a29be6225dec60417355b505
Muhammet Altindal <muhammet@altindal.de>
2013-09-21 01:23:41 +0200

    Combining both gcd implementation in one file.

--- basics/gcd.hs
+++ basics/gcd.hs
@@ -2,3 +2,8 @@ gcd_ u v
   | u == 0 = v
   | u < v = gcd_ v u
   | u >= v = gcd_ (u-v) v
+             
+gcdm u v
+  | u == 0 = v
+  | u < v = gcdm v u
+  | u >= v = gcdm (u `mod` v) v
--- basics/gcdm.hs
+++
@@ -1,4 +0,0 @@
-gcdm u v
-  | u == 0 = v
-  | u < v = gcdm v u
-  | u >= v = gcdm (u `mod` v) v
