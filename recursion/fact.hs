b128e69dd0547085ca0300aa14122f2f9a00991b
Muhammet Altindal <muhammet@altindal.de>
2013-10-03 17:06:29 +0200

    Implementing the factorial function in a recursive way.

--- recursion/fact.hs
+++ recursion/fact.hs
@@ -1,1 +1,4 @@
-fact n = product [1..n]
+-- given a number n calculate it's factorial
+-- where n is greater or equal to 0.
+fact :: (Eq a, Num a) => a -> a
+fact n = if n==0 then 1 else n * fact (n-1)
