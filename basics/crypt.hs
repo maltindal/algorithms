86003f09bad6204522061c1448a1912247e5eaed
Muhammet Altindal <muhammet@altindal.de>
2013-10-19 21:52:09 +0200

    Adding basic implementation of the caesar and vigenère cipher in haskell.

---
+++ basics/crypt.hs
@@ -0,0 +1,84 @@
+import Data.Char
+
+-- caeser cipher / rot-13
+-- given a text -> key -> alphabet return the encrypted text
+caeserEnc :: String -> Int -> String -> String
+caeserEnc t k a = [ enc (sa k a) c | c <- t ]
+  where
+    -- given an alphabet a containing pairs of original and encoded chars 
+    -- and a char c to encode return the encoded char
+    enc :: [(Char, Char)] -> Char -> Char
+    enc a c
+      | c == ' ' = ' '
+      | otherwise = let (_,w) = head (filter (\x -> let (v,_) = x in v==c) a)
+                    in w
+
+-- caeser cipher / rot-13
+-- given a text -> key -> alphabet return the decrypted text
+caeserDec :: String -> Int -> String -> String
+caeserDec t k a = [ dec (sa k a) c | c <- t ]
+  where
+    -- given an alphabet a containing pairs of original and encoded chars
+    -- and a char c to decode return the decoded char
+    dec :: [(Char, Char)] -> Char -> Char
+    dec a c
+        | c == ' ' = ' '
+        | otherwise = let (v,_) = head (filter (\x -> let (_,w) = x in w==c) a)
+                      in v
+
+-- vigenère cipher
+-- given a text -> key -> alphabet return the encrypted text
+vigenèreEnc :: String -> String -> String -> String
+vigenèreEnc t k a = foldl (++) "" 
+                    [ caeserEnc [c] (i kc a) a | (c,kc) <- zip t (kt t k) ]
+  where
+    -- given a text t and a key k
+    -- return a list containing pairs of char and its key char
+    kt :: String -> String -> String
+    kt t k = (++) (foldl (++)
+                        ""
+                        (replicate (div (length t) (length k)) k))
+                  (let (r,_) = splitAt (mod (length t) (length k)) k in r)
+
+-- TODO implement vigenère decryption function
+-- given a text -> key -> alphabet return the decrypted text
+--vigenèreDec :: String -> String -> String -> String
+--vigenèreDec t k a
+
+-- given an alphabet shift it k times to the left
+-- and return pairs containing the original and encoded char
+sa :: Int -> String -> [(Char, Char)]
+sa k a = let (x,y) = splitAt k a
+        in zip a (y ++ x)
+
+-- given a char c and a String s which contains c return the index of c
+i :: Char -> String -> Int
+i c s = let (_,n) = head (filter (\x -> let (a,_) = x in a ==c) 
+                                 (zip s [0..length s - 1]))
+        in n
+
+-- crypto analysis
+
+-- given a text t and an alphabet return the decrypted text
+breakCaeser :: String -> String -> String
+breakCaeser t a = head (filter (`cs` "die") (p t a))
+  where
+    -- given a text t and an alphabet a
+    -- return a list of possible decryptions
+    p :: String -> String -> [String]
+    p t a = [ caeserDec t k a | k <- [1..length a - 1] ]
+
+-- given a text t and a word w check if t contains w
+cs :: String -> String -> Bool
+cs t w = elem w (ws t (length w))
+
+-- given a text t -> length of word
+-- compute the words with length l at the indices 0..upper bound
+ws :: String -> Int -> [String]
+ws t l = [extr t i l | i <- [0..length t - l] ]
+
+-- given a text t -> index i -> word length l
+-- return the word at the length l at the index i
+extr :: String -> Int -> Int -> String
+extr t i l = let (_,b) = splitAt i t
+             in (let (a,_) = splitAt l b in a)
